87b4125522b65677af5b1dcec32f91be09551bd7
raul ramos <rramosp>
2021-12-01 10:18:27 -0500

    iteration

--- rlxutils/__init__.py
+++ rlxutils/__init__.py
@@ -1,4 +1,10 @@
-from .version import __version__
 from .rlxutils import *
 import rlxutils.optimization
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
 
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 import setuptools
 
-setuptools.setup(version_config=True, setup_requires=["setuptools-git-versioning"])
+#setuptools.setup(version_config=True, setup_requires=["setuptools-git-versioning"])
+
 
 
 setup(name='rlxutils',
       install_requires=['matplotlib','numpy', 'pandas','joblib',
                         'progressbar2', 'psutil'
                        ],
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
       scripts=[],
       packages=['rlxutils'],
       include_package_data=True,
