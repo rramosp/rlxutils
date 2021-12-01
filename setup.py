d74c3b7583ac4a367c3c2026ca1f177d2d2502cb
raul ramos <rramosp>
2021-12-01 09:54:50 -0500

    iteration

--- setup.py
+++ setup.py
@@ -5,15 +5,10 @@ setuptools.setup(version_config=True, setup_requires=["setuptools-git-versioning
 
 
 setup(name='rlxutils',
-      description='rlx general purpose utils',
-      url='http://github.com/rramosp/rlxutils',
       install_requires=['matplotlib','numpy', 'pandas','joblib',
                         'progressbar2', 'psutil'
                        ],
       scripts=[],
-      author='raul ramos',
-      author_email='rulix.rp@gmail.com',
-      license='MIT',
       packages=['rlxutils'],
       include_package_data=True,
       zip_safe=False)
