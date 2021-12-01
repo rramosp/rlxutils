34ca99e7b826bf498ec1fc60f7df45d2584d5a7e
raul ramos <rramosp>
2021-12-01 09:18:44 -0500

    iteration

--- rlxutils/rlxutils.py
+++ rlxutils/rlxutils.py
@@ -1,5 +1,8 @@
 from joblib import Parallel, delayed
 import sys, hashlib, subprocess
+import matplotlib.pyplot as plt
+import numpy as np
+from time import time
 
 class mParallel(Parallel):
     """
@@ -10,6 +13,78 @@ class mParallel(Parallel):
             fmsg = '[%s]: %s' % (self, msg % msg_args)
             sys.stdout.write('\r ' + fmsg)
             sys.stdout.flush()
+
+def subplots(elems, n_cols=None, usizex=3, usizey=3 ):
+    """
+    generates grid with a subplot for each elem in elems. for instance:
+
+       for ax,i in subplots(2):
+           if i==0:
+              ax.scatter(....)
+           if i==1:
+              ax.plot(...)
+
+
+    elems: an iterable, or an integer 
+    n_cols: the number of columns for the grid. If none it is set to min(num elems, 15)
+    usizex, usizey: size of each subplot
+    """
+    
+    if type(elems)==int:
+        elems = np.arange(elems) 
+
+    n_elems = len(elems)
+    
+    if n_cols is None:
+        n_cols = n_elems if n_elems<=15 else 15
+    
+    n_rows = n_elems//n_cols + int(n_elems%n_cols!=0)
+    fig = plt.figure(figsize=(n_cols*usizex, n_rows*usizey))
+    
+    for i in range(n_elems):
+        ax = fig.add_subplot(n_rows, n_cols, i+1)
+        yield ax, elems[i]
+
+class ElapsedTimes:
+    """
+    usage
+
+    et = ElapsedTimes():
+
+    with et("stage 1"):
+       ... some python code...
+
+    with et("stage 2"):
+       ... other python code...
+
+ 
+    print (et.elapsed_times)
+
+    """
+    def __init__(self):
+        self.start_times = {}
+        self.elapsed_times = {}
+
+    def __enter__(self):
+        self.start_times[self.current_key] = time()
+        return self        
+        
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        key = self.current_key
+        if key not in self.elapsed_times.keys():
+            self.elapsed_times[key] = 0
+
+        if key in self.start_times.keys():
+            self.elapsed_times[key] += time() - self.start_times[key]
+        
+    def __call__(self, key):
+        self.current_key = key
+        return self
+
+    def __repr__(self):
+        return str(self.elapsed_times)
+
+split = lambda a, chunk_size: [a[x:x+chunk_size] for x in np.arange(0, len(a), chunk_size)]
 
 def md5hash(s):
     """
