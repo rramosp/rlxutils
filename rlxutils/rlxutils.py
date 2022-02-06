d9823bdca388aa0f207dfef6791dda3c375981ca
raul ramos <rramosp@unal.edu.co>
2022-02-06 13:28:50 -0500

    added copy_func

--- rlxutils/rlxutils.py
+++ rlxutils/rlxutils.py
@@ -3,6 +3,20 @@ import sys, hashlib, subprocess
 import matplotlib.pyplot as plt
 import numpy as np
 from time import time
+import types
+import functools
+
+def copy_func(f):
+    """
+    creates a new function object copying an existing one
+    Based on http://stackoverflow.com/a/6528148/190597 (Glenn Maynard)
+    """
+    g = types.FunctionType(f.__code__, f.__globals__, name=f.__name__,
+                           argdefs=f.__defaults__,
+                           closure=f.__closure__)
+    g = functools.update_wrapper(g, f)
+    g.__kwdefaults__ = f.__kwdefaults__
+    return g
 
 class mParallel(Parallel):
     """
